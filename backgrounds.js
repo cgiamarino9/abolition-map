91838f9479760f7df689aef5d0bda8c8affaee1f
cgiamarino9 <68085870+cgiamarino9@users.noreply.github.com>
2021-01-12 15:52:39 -0800

    Add files via upload

---
+++ backgrounds.js
@@ -0,0 +1,47 @@
+var backgrounds = [
+  {
+    img: 'map',
+    startSlug: 'slide-0',
+    endSlug: 'slide-7'
+  },
+  {
+    img: 'https://wp.dailybruin.com/images/2020/02/web.news_.LGBTQhate.file_.jpg',
+    startSlug: 'slide-8',
+    endSlug: 'slide-9'
+  },
+  {
+    img: 'map',
+    startSlug: 'slide-10',
+    endSlug: 'slide-13'
+  },
+  {
+    img: 'https://www.ucla.edu/img/content-images/campus-life-housing-05.jpg',
+    startSlug: 'slide-14',
+    endSlug: 'slide-14'
+  },
+  {
+    img: 'map',
+    startSlug: 'slide-15',
+    endSlug: 'slide-15'
+  },
+  {
+    img:'https://www.bielskiservices.com/images/projects/schools/ucla/ucla_haines_hall.JPG',
+    startSlug:'slide-16',
+    endSlug:'slide-16'
+  },
+  {
+    img: 'map',
+    startSlug: 'slide-17',
+    endSlug: 'slide-18'
+  },
+  {
+    img:'https://s3.amazonaws.com/cms.ipressroom.com/173/files/201911/5de6ee8e2cfac209ff5f7847_LawSchool/LawSchool_0e2728c5-a1d5-4cb8-baba-17391d2b8a8a-prv.jpg',
+    startSlug:'slide-19',
+    endSlug:'slide-19'
+  },
+  {
+    img: 'map',
+    startSlug: 'slide-20',
+    endSlug: 'slide-22'
+  }
+]
